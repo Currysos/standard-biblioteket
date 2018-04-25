77981ab70c96c0f49974afa44ca2ffdf297db4c4
itggot-edvin-alestig <edvin.alestig@itggot.se>
2018-04-25 14:07:44 +0200

    between_strict

---
+++ lib/between_strict.rb
@@ -0,0 +1,25 @@
+# Public: Takes three Integers and determines if the first one is in between the second and third.
+#
+# number - The Integer to check
+# lower - The lower Integer
+# upper - The upper Integer
+# 
+# Examples
+#   between_strict(-4, 0, 52)
+#   # => false
+#   between_strict(19, 4, 27)
+#   # => true
+#
+# Returns the result 
+def between(number, lower, upper)
+    if !number.is_a?(Integer) || !lower.is_a?(Integer) || !upper.is_a?(Integer)
+        return nil
+    else 
+        if number < upper
+            if number > lower
+                return true
+            end
+        end
+        return false
+    end
+end
