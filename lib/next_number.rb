129690a4e6f44d82bfbb034c3ec53336fe71ee74
itggot-edvin-alestig <edvin.alestig@itggot.se>
2018-04-25 13:32:41 +0200

    Added documentation

--- lib/next_number.rb
+++ lib/next_number.rb
@@ -1,3 +1,12 @@
+# Public: Takes an Integer and returns the next Integer.
+#
+# number - The Integer to add one to
+# 
+# Examples
+#   next_number(4)
+#   # => 5
+#
+# Returns the next Integer
 def next_number(number)
     if !number.is_a?(Integer)
         return false
